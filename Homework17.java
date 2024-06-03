d6bac457cdfa6d251896b54019553765e210e0fe
Aidnim <india.messam@testpro.io>
2024-06-02 21:10:39 -0400

    I'm lost
    I will have to see how to do this in tomorrow's class.

--- Homework17.java
+++ Homework17.java
@@ -1,8 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.internal.BaseTestMethod;
 
 import java.time.Duration;
 
@@ -11,16 +17,66 @@ public class Homework17 extends BaseTest {
     @Test
     public void addSongToPlaylist(){
 
-        //      Added ChromeOptions argument below to fix websocket error
+       // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+
+        // Initialize ChromeDriver with options
         ChromeOptions options = new ChromeOptions();
-
+        options.addArguments("--start-maximized");
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        try {
+            // Open the URL
+            driver.get("https://qa.koel.app/");
+
+            // Wait for the page to load and the login form to be visible
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("email")));
+
+            // Locate the email and password fields
+            WebElement emailField = driver.findElement(By.name("email"));
+            WebElement passwordField = driver.findElement(By.name("password"));
+
+            // Enter credentials
+            emailField.sendKeys("india.messam@testpro.io");
+            passwordField.sendKeys("slcTalgy");
+
+            // Locate and click the login button
+            WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+            loginButton.click();
+
+            // Search for a song
+            WebElement sideSearchShowing = driver.findElement(By.cssSelector("[class='side search showing']"));
+            sideSearchShowing.click();
+
+            sideSearchShowing = driver.findElement(By.name,"q"());
+            sideSearchShowing.sendKeys("love");
+            sideSearchShowing.sendKeys(Keys.RETURN);
+
+
+            //click view all button
+
+            //click first song in search results
+
+            //click add to button
+
+            //choose playlist to add
+
+            //Verify that a notation message appears containing Actual playlist name
+
+            //use Assert.assertEquals()
+
+            System.out.println("Login test passed!");
+
+        } catch (Exception e) {
+            System.out.println("Login test failed!");
+            e.printStackTrace();
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
+    }
+}
 
     }
 
